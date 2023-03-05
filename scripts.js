7c0aae5a92295ef252885f27296ad1c96bde4205
Dani Funes <funesfuentesdaniel@gmail.com>
2023-03-05 18:24:57 +0100

    Cambio PC

--- scripts.js
+++ scripts.js
@@ -31,36 +31,81 @@ const militaryHelicopter = [
 ];
 
 class Gallery {
-    constructor(civilImages, militaryImages) {
+    constructor(civilImages, militaryImages) {  
+        this.civilImages = civilImages;
+        this.militaryImages = militaryImages;
     }
     
     getRandomCivil() {
+        const randomNumber = Math.floor(Math.random() * this.civilImages.length)
+        return this.civilImages[randomNumber];
     }
     
     getRandomMilitary() {
+        const randomNumber = Math.floor(Math.random() * this.militaryImages.length)
+        return this.militaryImages[randomNumber];
     }
     
     getAll() {
-    }
+            return this.civilImages;
+        }       
+    
 }
 
 class Painter {
+    
+    
+
     constructor() {
+        return this.createGallery();  
     }
 
     createGallery() {
+        this.gallery = document.createElement("section");
+        document.body.appendChild(this.gallery);
     }
 
+
     createImageTag(imageUrl) {
+        return `<picture><img src="${imageUrl}"/></picture>`
+
     }
 
     paintSingleImage(imageUrl) {
+        this.gallery += this.createImageTag(imageUrl);
+        return this.gallery;
     }
 
     paintMultipleImages(arrayOfImages) {
+        for(let i=0; i<arrayOfImages.length ;i++) {
+            this.gallery += this.createImageTag(arrayOfImages);
+            return this.gallery;
+        }
+         
     }
+
+
 }
 
 const aircrafts = new Gallery(civilAircrafts, militaryAircrafts);
 const helicopters = new Gallery(civilHelicopters, militaryHelicopter);
 const painter = new Painter();
+
+// console.log (aircrafts.militaryImages[4])
+// console.log (helicopters.getAll());
+// painter.createGallery();
+// painter.paintSingleImage('assets/helicoptero-civil-4.jpg');
+// painter.paintSingleImage('assets/helicoptero-civil-3.jpg')
+// console.log(painter.gallery)
+// painter.paintMultipleImages(militaryAircrafts)
+// console.log(painter.paintSingleImage(aircrafts.getRandomCivil()))
+// console.log(painter.gallery)
+
+// console.log(aircrafts.getAll())
+// console.log(typeof aircrafts.getAll())
+// console.log()
+// console.log(aircrafts.getAll())
+// console.log(painter.paintMultipleImages(aircrafts.getAll()))
+// console.log(painter.paintSingleImage(aircrafts.getRandomCivil()))
+
+console.log(aircrafts.civilImages)
